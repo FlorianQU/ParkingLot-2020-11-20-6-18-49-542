45ad51d988465910559b010b0da7728e9664fc3a
hockeyqqy <qqu@slb.com>
2020-11-22 17:37:50 +0800

    feat: manager can appoint parkingboy to park and fetch correctly

--- ParkingLotTest/ParkingLotManagerTest.cs
+++ ParkingLotTest/ParkingLotManagerTest.cs
@@ -40,5 +40,24 @@ namespace ParkingLotTest
             var carReturned = newCustomer.FetchCarFromParkingBoy(manager, newCustomer.GetTicket(0));
             Assert.Equal(car, carReturned);
         }
+
+        [Fact]
+        public void Manger_Should_Appoint_ParkingBoy_To_Park_And_Fetch()
+        {
+            //given
+            var car = new Car("car_1");
+            var newCustomer = new Customer("customer_1", car);
+            var parkingLot = new ParkingLot.ParkingLot("parkingLot_1", 3);
+            var parkingBoy = new ParkingBoy("parkingBoy_1", parkingLot);
+            var manager = new ParkingLotServiceManager("manager_1", parkingLot, new List<ParkingBoy>(new[] { parkingBoy }));
+
+            //when
+            var parkingResult = manager.ParkCarByParkingBoy(car, parkingBoy, out var parkErrorMessage);
+            Assert.Equal("parkingLot_1", parkingResult.ParkingLotId);
+            Assert.Equal("car_1", parkingResult.CarId);
+
+            var carResult = manager.FetchCarByParkingBoy(parkingResult, parkingBoy, out var fetchErrorMessage);
+            Assert.Equal(car, carResult);
+        }
     }
 }
